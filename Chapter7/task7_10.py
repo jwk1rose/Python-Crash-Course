601cfa5a0dfef32f891a97ed69a2029d6364f368
jwk1rose <2287924124@qq.com>
2022-12-18 12:22:58 +0800

    第七章习题最后一题完成

--- Chapter7/task7_10.py
+++ Chapter7/task7_10.py
@@ -3,7 +3,6 @@ if_continue = True
 while if_continue:
     user_name = input("What is your name?")
     user_input = input("IF you can arrival any where you want,where you want most?")
-
+    result[user_name.strip()] = user_input
     if_continue = input("if continue? (Y/N)").upper() == 'Y'
-
 print(result)
