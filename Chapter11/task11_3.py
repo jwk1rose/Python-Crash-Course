3c4553c314df762ba371b98c7d9cb7341101d1c0
jwk1rose <2287924124@qq.com>
2022-12-29 10:56:22 +0800

    第十一章习题结束 新冠耽误了很多天

--- Chapter11/task11_3.py
+++ Chapter11/task11_3.py
@@ -7,10 +7,18 @@ class Employee:
         self.last_name = last_name
         self.salary = salary
 
-    def gve_raise(self, d_salary=5000):
+    def give_raise(self, d_salary=5000):
         self.salary += d_salary
+        return self.salary
 
 
 class Tempolory(unittest.TestCase):
-   def setUp(self) -> None:
-       self.assertEqual(s)
+    def setUp(self) -> None:
+        self.my_employee = Employee('zhang', 'zheng', 0)
+
+    def test_give_default_raise(self):
+        self.assertEqual(5000, self.my_employee.give_raise())
+
+    def test_give_custom_raise(self):
+        given_raise = 1000
+        self.assertEqual(given_raise, self.my_employee.give_raise(given_raise))
